0b886bd3cbebe9761c80b7e0b42360ca1a782991
Emre Bogazliyanlioglu <ebgzly@gmail.com>
2024-05-02 15:57:48 +0300

    Adding existing function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,75 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import { SpeechClient, protos } from "@google-cloud/speech";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
+const speechClient = new SpeechClient();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.transcribeAudio = functions.firestore
+  .document("users/{userId}/tests_taken/{testId}")
+  .onCreate(async (snap, context) => {
+    const {userId} = context.params;
+    const documentData = snap.data();
+    const audioUrls: string[] = [
+      documentData.audio1,
+      documentData.audio2,
+      documentData.audio3,
+      documentData.audio4,
+    ].filter((url): url is string => typeof url === "string");
+
+    const transcriptions: Array<{
+      transcription: string;
+      confidence: number;
+    }> = [];
+
+    for (const url of audioUrls) {
+      const audio: protos.google.cloud.speech.v1.IRecognitionAudio = {uri: url};
+      const config: protos.google.cloud.speech.v1.IRecognitionConfig = {
+        encoding: protos.google.cloud.speech.v1.RecognitionConfig.AudioEncoding
+          .LINEAR16,
+        sampleRateHertz: 48000,
+        languageCode: "en-US",
+        enableWordConfidence: true,
+        enableAutomaticPunctuation: true,
+      };
+      try {
+        const [operation] = await speechClient.longRunningRecognize({
+          audio,
+          config,
+        });
+        const [response] = await operation.promise();
+        if (response.results) {
+          const transcriptionData = response.results
+            .map(
+              (result: protos.google.cloud.speech.v1.ISpeechRecognitionResult
+              ) => ({
+                transcription: result.alternatives?.[0].transcript || "",
+                confidence: result.alternatives?.[0].confidence || 0,
+              }))
+            .reduce(
+              (
+                {transcription, confidence},
+                {transcription: t, confidence: c},
+                _,
+                array
+              ) => ({
+                transcription: `${transcription}\n${t}`.trim(),
+                confidence: confidence + (c / array.length),
+              }),
+              {transcription: "", confidence: 0},
+            );
+
+          transcriptions.push(transcriptionData);
+        }
+      } catch (error) {
+        console.error(`Failed to transcribe ${url}:`, error);
+      }
+    }
+
+    if (transcriptions.length > 0) {
+      await admin.firestore()
+        .doc(`users/${userId}/audio_text/${snap.id}`)
+        .set({transcriptions});
+    }
+  });
